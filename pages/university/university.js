91b89a4688bb9f718b011e87692a77a0706eb697
Chenhao Zhang <chenhao3@illinois.edu>
2018-07-02 10:00:49 +0800

    UI change

--- pages/university/university.js
+++ pages/university/university.js
@@ -68,23 +68,6 @@ Page({
   onShareAppMessage: function () {
   
   },
-
-  showInput: function () {
-    this.setData({
-      inputShowed: true
-    });
-  },
-  hideInput: function () {
-    this.setData({
-      inputVal: "",
-      inputShowed: false
-    });
-  },
-  clearInput: function () {
-    this.setData({
-      inputVal: ""
-    });
-  },
   inputTyping: function (e) {
     this.setData({
       inputVal: e.detail.value
