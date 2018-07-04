391248f02a1d37fca489c45b233f3fc61eaf8bcf
Louis-He <1726110778@qq.com>
2018-07-04 17:43:19 +0800

    add low quality network situation notification page. improve user page.

--- pages/user/user.js
+++ pages/user/user.js
@@ -7,9 +7,11 @@ Page({
    * 页面的初始数据
    */
   data: {
+    debug: false,
     isAgree: false,
     firstViewmsg: "",
     firstView: true,
+    bothTrue: false,
     username: "叮咚的雨",
     rp: 20,
     university: "University of Toronto",
@@ -23,51 +25,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-    var that = this
-    wx.getStorage({
-      key: 'firstView',
-      success: function (res) {
-        console.log("onLoad: ", res.data)
-        if (res.data){
-          // 首次未进入小程序
-          that.setData({
-            firstViewmsg: "决定加入桥选学生社群"
-          })
-        }else{
-          // 已经进入过小程序，直接进入用户页
-          that.setData({
-            'firstView': false
-          })
-          that.userPageOnLoad();
-        }
-      } ,
-      fail: function (res){
-        console.log('fail get storage status')
-        wx.getUserInfo({
-          /* 之前授权过，直接进入*/
-          success: function (res){
-            wx.setStorage({
-              key: "firstView",
-              data: false
-            })
-            that.setData({
-              "firstView": false,
-            })
-          },
-          /* 否则视作新用户，请求授权 */
-          fail: function (res){
-            wx.setStorage({
-              key: "firstView",
-              data: true
-            })
-            that.setData({
-              "firstView": true,
-              "firstViewmsg": "欢迎加入桥选学生社群"
-            })
-          }
-        })
-      }
-    })
+    
   },
 
   /**
@@ -81,7 +39,64 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    var that = this
+    wx.getStorage({
+      key: 'firstView',
+      success: function (res) {
+        console.log("onLoad: ", res.data)
+        if (res.data) {
+          // 首次未进入小程序
+          console.log('非首次进入小程序首页')
+          that.setData({
+            firstViewmsg: "决定加入桥选学生社群"
+          })
+        } else {
+          // 已经进入过小程序，直接进入用户页
+          console.log('已进入过小程序')
+          that.setData({
+            'firstView': false
+          })
+          that.userPageOnLoad();
+        }
+      },
+      fail: function (res) {
+        console.log('小程序无storage')
+        // 鉴定用户是否给予权限
+        wx.getSetting({
+          success: function (res) {
+            if (res.authSetting['scope.userInfo']) {
+              console.log('用户已授权，直接进入')
+              // 已经授权，直接进入小程序
+              wx.setStorage({
+                key: "firstView",
+                data: false
+              })
+              that.setData({
+                "firstView": false,
+              })
+            } else {
+              console.log('用户未授权，未进入过小程序')
+              // 否则视作新用户，请求授权
+              wx.setStorage({
+                key: "firstView",
+                data: true
+              })
+              that.setData({
+                "firstView": true,
+                "firstViewmsg": "欢迎加入桥选学生社群"
+              })
+            }
+          },
+          fail: function (res) {
+            /* 网络环境不佳 */
+            that.lowQualityNetwork();
+          },
+          complete: function () {
+
+          }
+        })
+      }
+    })
   },
 
   /**
@@ -124,74 +139,101 @@ Page({
    */
   firstUse: function() {
     var that = this;
-    wx.getUserInfo({
-      success: function (res){
-        console.log("SUCCESS")
-        wx.setStorage({
-          key: "firstView",
-          data: false
-        })
-        that.setData({
-          "firstView": false
-        })
-        that.userPageOnLoad();
-      },
-      fail: function (res){
-        if(!that.isAgree){
-          wx.showModal({
-            content: '请阅读并同意相关条款以继续',
-            showCancel: false,
-            success: function (res) {
-              if (res.confirm) {
-                console.log('用户点击确定')
-              }
-            }
-          });
-        }else{
-          wx.showModal({
-            content: '请同意巧选校园使用您的基本信息以继续',
-            showCancel: false,
-            success: function (res) {
-              if (res.confirm) {
-                console.log('用户点击确定')
-              }
-            }
-          });
-        }
-      }
+    
+    wx.setStorage({
+      key: "firstView",
+      data: false
     })
+    that.setData({
+      "firstView": false
+    })
+    that.userPageOnLoad();  
   },
 
+  // 同意条款单选框
   bindAgreeChange: function (e) {
+    var that = this;
     this.setData({
       isAgree: !!e.detail.value.length
     });
+
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          that.setData({
+            'bothTrue': true
+          })
+        }else{
+          wx.showModal({
+            content: '请先点击蓝色按钮先给予我们获取您基本信息的权限以保证正常使用',
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+                console.log('用户确认去授权')
+              }
+            }
+          });
+          that.setData({
+            isAgree: false
+          });
+        }
+      },
+      fail: function(res){
+
+      }
+    })
   },
 
   userPageOnLoad: function(){
     var that = this;
-    wx.getUserInfo({
-      success: function (res){
-        console.log(res.rawData)
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，直接获取用户信息
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.rawData)
+            },
+            fail: function (res) {
+              console.log("fail")
+              wx.showModal({
+                content: '授权状态发生更改，请重新给予我们获取基本信息的权限以保证正常使用',
+                showCancel: false,
+                success: function (res) {
+                  if (res.confirm) {
+                    console.log('用户确认返回首页重新确认权限')
+                  }
+                }
+              });
+              that.setData({
+                "firstView": true,
+                "isAgree": false,
+                "firstViewmsg": "继续使用巧选校园"
+              })
+            }
+          })
+        } else {
+          console.log("WTF不应该出现的console log？！")
+        }
       },
       fail: function (res){
-        console.log("fail")
-        wx.showModal({
-          content: '授权状态发生改版，请重新确认给予获取您基本信息的权限',
-          showCancel: false,
-          success: function (res) {
-            if (res.confirm) {
-              console.log('用户点击确定')
-            }
-          }
-        });
-        that.setData({
-          "firstView": true,
-          "isAgree": true,
-          "firstViewmsg": "继续使用巧选校园"
-        })
+        that.lowQualityNetwork();
       }
     })
+  },
+
+  // 警告用户网络环境差
+  lowQualityNetwork: function(){
+    if (!this.debug) {
+      this.setData({
+        "firstView": true,
+        "isAgree": false,
+        "firstViewmsg": "使用巧选校园"
+      })
+      wx.navigateTo({
+        url: 'lowQualityNetwork',
+      })
+    }
   },
 
   /**
