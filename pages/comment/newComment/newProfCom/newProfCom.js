82170915b433f3a52a0c5a176862ea33ea4c26ab
Louis-He <1726110778@qq.com>
2018-07-25 09:51:56 +0800

    add new profComment page and partly complete. Binding functions are still needed to implement

--- pages/comment/newComment/newProfCom/newProfCom.js
+++ pages/comment/newComment/newProfCom/newProfCom.js
@@ -8,14 +8,16 @@ Page({
     is_modal_Hidden: true,
     is_modal_Msg: '我是一个自定义组件',
     is_modal_Title: '提示',
-  
+    "grades": ["请选择", "A", "B", "C","D","F","Dropped the course"],
+    "gradeIndex": 0,
+    "grade": null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setScrollHeight();
   },
 
   /**
@@ -79,5 +81,19 @@ Page({
     });
   },
 
-  
+  bindGradeChange: function (e) {
+    var that = this;
+    console.log('picker country 发生选择改变，携带值为', this.data.grades[e.detail.value]);
+
+    this.setData({
+      'grade': that.data.grades[e.detail.value],
+      'gradeIndex': e.detail.value
+    })
+  },
+
+  nextStep: function (){
+    wx.navigateTo({
+      url: 'detailCom',
+    })
+  }
 })
