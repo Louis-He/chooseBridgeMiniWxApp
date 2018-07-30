bc1f4949b92119567a51ee88f14e5a3efe92750f
Louis-He <1726110778@qq.com>
2018-07-30 19:47:10 +0800

    link add university to server

--- utils/requestUtil.js
+++ utils/requestUtil.js
@@ -1,7 +1,10 @@
 var utilMd5 = require('util.js'); 
 
+// 以下为用户信息请求函数
 /**
  *  得到用户UnionID
+ *  传入数据：firstRequestedData， tmpUserInfo
+ *  函数类型：回掉函数
  *  注意： 传入firstRequestedData和tmpUserInfo
  */
 function getUserUnionID(firstRequestedData, tmpUserInfo, _callback) {
@@ -45,8 +48,9 @@ function getUserUnionID(firstRequestedData, tmpUserInfo, _callback) {
 
 /**
  * 得到用户在ChooseBridge服务器上的个人信息
- * 用法：
- * requestUtil.getChooseBridgeUserInfo(unionId, function(res){
+ * 传入数据：unionId
+ * 函数类型：回掉函数
+ * 用法：requestUtil.getChooseBridgeUserInfo(unionId, function(res){
               })
  */
 function getChooseBridgeUserInfo(unionID, _callback){
@@ -68,8 +72,9 @@ function getChooseBridgeUserInfo(unionID, _callback){
 
 /**
  * 传入用户entities->id，返回用户viewmycourses中的个人信息
- * 用法：
- * getViewmycoursesUserInfo(userid, function(res){
+ * 传入数据：userid
+ * 函数类型：回掉函数
+ * 用法：getViewmycoursesUserInfo(userid, function(res){
               })
  */
 function getViewmycoursesUserInfo(userid, _callback){
@@ -96,6 +101,109 @@ function getViewmycoursesUserInfo(userid, _callback){
 }
 
 /**
+ * 传入用户entities->id，返回用户viewmycourses中的Token
+ * 传入数据：userid
+ * 函数类型：回掉函数
+ * 用法：getViewmycoursesToken(userid, function(res){
+              })
+ */
+function getViewmycoursesToken(userid, _callback) {
+  var that = this;
+  var explicitData = { "ucenter_id": userid };
+  var requestData = { "ucenter_id": userid, "sign": that.getSign(explicitData) };
+
+  wx.request({
+    url: 'https://api.viewmycourses.com/api/wechat/login',
+    method: 'POST',
+    data: requestData,
+    success: function (res) {
+      _callback(res.data.token);
+    }
+  })
+}
+
+// 以下为非用户信息请求很熟
+/**
+ * 得到支持的所有国家地区列表
+ * 传入数据：无需
+ * 函数类型：回掉函数
+ */
+function getCountries(_callback){
+  wx.request({
+    url: 'https://api.viewmycourses.com//open-api/geo/get-all-countrys',
+    method: 'GET',
+    success: function(res){
+      var arrayLength = res.data.data.length;
+      var countryList = new Array()
+      var i = 0;
+      for (i = 0; i < arrayLength; i++) {
+        countryList[i] = res.data.data[i].country_name
+      }
+      // console.log(countryList)
+      
+      _callback(countryList)
+    }
+  })
+}
+
+/**
+ * 得到特定国家支持的所有省份或联邦州列表
+ * 传入数据：countryID
+ * 函数类型：回掉函数
+ * 注意：countryID为在getCountries()函数中获取的countryID
+ */
+function getProvinceByCountry(countryID, _callback) {
+  // console.log(countryID)
+  wx.request({
+    url: 'https://api.viewmycourses.com//open-api/geo/get-province-by-country',
+    method: 'POST',
+    data: {"country_id": countryID},
+    success: function (res) {
+      // console.log(res)
+      var arrayLength = res.data.data.length;
+      var provinceList = new Array();
+      var provinceIndicesList = new Array();
+      var i = 0;
+      for (i = 0; i < arrayLength; i++) {
+        provinceList[i] = res.data.data[i].province_name;
+        provinceIndicesList[i] = res.data.data[i].province_id;
+      }
+      // console.log(provinceList)
+      _callback([provinceIndicesList, provinceList])
+    }
+  })
+}
+
+/**
+ * 得到特定地区支持的所有城市列表
+ * 传入数据：provinceID
+ * 函数类型：回掉函数
+ * 注意：provinceID为在getProvinceByCountry()函数中获取的provinceID
+ */
+function getCityByProvince(provinceID, _callback) {
+  // console.log(provinceID)
+  wx.request({
+    url: 'https://api.viewmycourses.com//open-api/geo/get-city-by-province',
+    method: 'POST',
+    data: { "province_id": provinceID },
+    success: function (res) {
+      // console.log(res)
+      var arrayLength = res.data.data.length;
+      var cityList = new Array();
+      var cityIndicesList = new Array();
+      var i = 0;
+      for (i = 0; i < arrayLength; i++) {
+        cityList[i] = res.data.data[i].city_name;
+        cityIndicesList[i] = res.data.data[i].city_id;
+      }
+      // console.log(provinceList)
+      _callback([cityIndicesList,cityList])
+    }
+  })
+}
+
+// 以下为辅助函数
+/**
  * 加密工具函数
  * 对于inputData进行md5加密
  */
@@ -112,6 +220,10 @@ module.exports = {
   getChooseBridgeUserInfo: getChooseBridgeUserInfo,
   getUserUnionID: getUserUnionID,
   getViewmycoursesUserInfo: getViewmycoursesUserInfo,
+  getViewmycoursesToken: getViewmycoursesToken,
+  getCountries: getCountries,
+  getProvinceByCountry: getProvinceByCountry,
+  getCityByProvince: getCityByProvince,
   getSign: getSign
 }
 
