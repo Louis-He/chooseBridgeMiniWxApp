27725db232e71ad85be78802546e8aa3396f3b1b
Louis-He <1726110778@qq.com>
2018-08-01 13:02:57 +0800

    improve

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 //app.js
-var requestUtil = require('utils/requestUtil.js'); 
-
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -9,14 +7,11 @@ App({
     wx.setStorageSync('logs', logs)
 
     // 登录
-    /*
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        
       }
     })
-    */
     // 获取用户信息
     wx.getSetting({
       success: res => {
