666794788a001168a612c3ceb879b8f77c700571
Chenhao Zhang <chenhaozhang@MacBook-Pro-2.local>
2018-08-04 17:23:50 +0800

    教授部分对接

--- pages/courses/specificPro/specificPro.js
+++ pages/courses/specificPro/specificPro.js
@@ -8,20 +8,15 @@ Page({
   data: {
     inputShowed: false,
     inputVal: "",
-    proName: "Effrosyni Seitaridou",
-    uniName: "University of Illinois at Urbana-Champaign",
-    schoolName: "College of Agricultural, Consumer & Environmental Sci.",
-    numOfLikes: "999+",
     tagsDown: [
       { tag: '和蔼可亲', num: '(99+)' },
       { tag: '公平公正', num: '(99+)' },
       { tag: '反馈及时', num: '(99+)' },
       { tag: '课后沟通多', num: '(99+)'}
     ],
-    workingIndex: '3.2',
     arrowTouched: false,
     hide: true,
-    commentTimee: '2018-06',
+    btmRates: [],
   },
   /**
   * 生命周期函数--监听页面加载
@@ -40,41 +35,63 @@ Page({
           for (var i = 0; i < result.coursesInfo.length; i++) {
             tempEffort += parseFloat(result.coursesInfo[i].effort);
           }
-          tempEffort = tempEffort / result.coursesInfo.length;
+          tempEffort = (tempEffort / result.coursesInfo.length).toFixed(1);
           that.setData({
             avgEffort: tempEffort,
           })
 
           //截取点评时间年月
           var length = result.rateInfo.length;
-          var tempCommentTime;
+          var tempCommentTime = new Array();
           for (var i = 0; i < length; i++) {
-            tempCommentTime = result.rateInfo[i].created_at.date.substring(0, 7);
+            tempCommentTime.push(result.rateInfo[i].created_at.date.substring(0, 7));
           }
 
           //转换tags数据结构
           var tempTags = new Array();
           for (var key in result.tagsInfo) {
             tempTags.push({ tag: key, num: '(' + result.tagsInfo[key] + ')' });
+            if (tempTags.length == 4) break;
           }
+          var tempTagsDown = new Array();
+          var i = 0;
+          for (var key in result.tagsInfo) {
+            if (i < 4) break;
+            tempTagsDown.push({ tag: key, num: '(' + result.tagsInfo[key] + ')' });
+          }
+          i++;
           that.setData({
-            tagsUp: tempTags
+            tagsUp: tempTags,
+            tagsDown: tempTagsDown
           })
 
           //转换用户点评数字成文字含义
           var temprate = result.rateInfo;
-          var tempDifficult = "";
+          var tempDifficult;
           var tempHomework;
           var tempAttendence;
           for (var i = 0; i < temprate.length; i++) {
-            if (parseInt(temprate.difficult_level) == 4) {
-              tempDifficult = "较难";
-              console.log("eyeyey");
+            if (temprate[i].difficult_level == 1) tempDifficult = "简单";
+            if (temprate[i].difficult_level == 2) tempDifficult = "较易";
+            if (temprate[i].difficult_level == 3) tempDifficult = "中等";
+            if (temprate[i].difficult_level == 4) tempDifficult = "较难";
+            if (temprate[i].difficult_level == 5) tempDifficult = "很难";
+            if (temprate[i].homework_num == 1) tempHomework = "很少";
+            if (temprate[i].homework_num == 2) tempHomework = "较少";
+            if (temprate[i].homework_num == 3) tempHomework = "中等";
+            if (temprate[i].homework_num == 4) tempHomework = "较多";
+            if (temprate[i].homework_num == 5) tempHomework = "很多";
+            if (temprate[i].is_attend == 1) tempAttendence = "是";
+            if (temprate[i].is_attend == 2) tempAttendence = "否";
+            that.data.btmRates[i] = {
+              difficult: tempDifficult,
+              homework: tempHomework,
+              attendence: tempAttendence,
             }
           }
-          console.log(temprate);
-          console.log(tempDifficult);
-          console.log(temprate.difficult_level);
+          that.setData({
+            btmRates: that.data.btmRates,
+          })
 
           //存储后台数据
           that.setData({
@@ -125,6 +142,18 @@ Page({
     this.setData({
       arrowTouched: false
     })
+  },
+  toResult: function () {
+    var that = this;
+    wx.setStorage({
+      key: 'professorName',
+      data: that.data.inputVal,
+      success: function() {
+        wx.switchTab({
+          url: '../courses',
+        })
+      }
+    });
   },
   commentCourse: function () {
     wx.navigateTo({
