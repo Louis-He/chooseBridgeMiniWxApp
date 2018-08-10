90721ba8bb09467d122c03df81b4f5d4b9f2e765
Louis-He <1726110778@qq.com>
2018-08-10 21:06:16 +0800

    reconstruct share func

--- pages/university/university.js
+++ pages/university/university.js
@@ -22,6 +22,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showShareMenu({
+      withShareTicket: true,
+      success: function (res) {
+      }
+    })
     this.setScrollHeight();
     var that = this;
     wx.getStorage({
