6fd5d92e4d07b5420b0344026de9ff95cf6778c1
Chenhao Zhang <chenhao3@illinois.edu>
2018-08-18 21:16:33 +0800

    UI bug fixed

--- pages/courses/specificPro/commentDetail/commentDetail.js
+++ pages/courses/specificPro/commentDetail/commentDetail.js
@@ -12,6 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    
     wx.showShareMenu({
       withShareTicket: true,
       success: function (res) {
@@ -91,7 +92,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    
   },
 
   /**
